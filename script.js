2857ea5621fd8b272a55c182314d4c3e28349058
awidney <adamwidney@hotmail.ca>
2022-06-08 16:18:51 -0700

    Create playRound function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function computerPlay() { // randomly returns either 'rock', 'paper' or 'scissors'
+// randomly returns either 'rock', 'paper' or 'scissors'
+function computerPlay() { 
    if (Math.floor(Math.random() * 100) <= 33) {
        return 'rock';
    } else if (Math.floor(Math.random() * 100) <= 66) {
@@ -7,3 +8,26 @@ function computerPlay() { // randomly returns either 'rock', 'paper' or 'scissor
        return 'scissors';
    }
 }
+
+// determines round result
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection) {
+        return 'Tie!';
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
+        return 'You Lose! Paper beats Rock!';
+    } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        return 'You Win! Paper beats Rock!';
+    } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+    } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    } else {
+        return 'You Win! Scissors beats Paper';
+    }
+}
+
+const playerSelection = prompt();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
